4a54f4ae81b78c18169dbdce014b7c47201703d3
beacagg <beatrizcaggianob@gmail.com>
2019-06-20 21:10:22 -0300

    update

---
+++ 01 Rest With Spring Boot/rest-course/src/main/java/br/com/springboot/restcourse/RestcourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.restcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcourseApplication.class, args);
+	}
+
+}
